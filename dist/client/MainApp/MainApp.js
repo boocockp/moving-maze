918aa10aad23391ac012e97967734f503298518a
boocockp <>
2024-08-28 11:55:51 +0100

    Variable obstacle points

--- dist/client/MainApp/MainApp.js
+++ dist/client/MainApp/MainApp.js
@@ -7,17 +7,19 @@ const MainPage_BoardPiecesItem = React.memo(function MainPage_BoardPiecesItem(pr
     const pathTo = name => props.path + '.' + name
     const parentPathWith = name => Elemento.parentPath(props.path) + '.' + name
     const {$item, $itemId, $index, $selected, onClick} = props
-    const {ItemSetItem, Icon} = Elemento.components
-    const {If, Or} = Elemento.globalFunctions
+    const {ItemSetItem, TextElement, Icon} = Elemento.components
+    const {If, Or, And} = Elemento.globalFunctions
     const _state = Elemento.useGetStore()
     const Columns = _state.useObject(parentPathWith('Columns'))
     const FromPosition = _state.useObject(parentPathWith('FromPosition'))
     const ToPosition = _state.useObject(parentPathWith('ToPosition'))
     const canDragItem = undefined
-    const styles = elProps(pathTo('BoardPieces.Styles')).width(100 / Columns + '%').border('1px solid blue').backgroundColor(If(Or($itemId == FromPosition, $itemId == ToPosition), 'orange', 'white')).aspectRatio('1').boxSizing('border-box').position('relative').props
+    const styles = elProps(pathTo('BoardPieces.Styles')).width(100 / Columns + '%').border('1px solid blue').backgroundColor(If(Or($itemId == FromPosition, $itemId == ToPosition), 'orange', () => If($itemId < Columns, 'lightgreen', 'white'))).aspectRatio('1').boxSizing('border-box').position('relative').props
 
     return React.createElement(ItemSetItem, {path: props.path, item: $item, itemId: $itemId, index: $index, onClick, canDragItem, styles},
+        React.createElement(TextElement, elProps(pathTo('Obstacle')).show(And($item != 'player', $item != 'empty')).styles(elProps(pathTo('Obstacle.Styles')).color('white').height('60%').width('60%').fontSize('2.1em').translate('-50% -50%').position('absolute').top('50%').left('50%').textAlign('center').borderRadius('5px').backgroundColor(`hsl(${$item * 40}deg, 50%, 50%)`).props).content($item).props),
         React.createElement(Icon, elProps(pathTo('PieceIcon')).iconName(If($item == 'obstacle', 'coronavirus', () => If($item == 'player', 'sentiment_very_satisfied'))).styles(elProps(pathTo('PieceIcon.Styles')).position('absolute').top('50%').left('50%').translate('-50% -50%').color(If($item == 'obstacle', 'red', () => If($item == 'player', 'green'))).fontSize('3em').width('60%').backgroundColor(If($item == 'obstacle', 'green')).height('60%').paddingTop('0%').props).props),
+        React.createElement(Icon, elProps(pathTo('PlayerIcon')).iconName('sentiment_very_satisfied').show($item == 'player').styles(elProps(pathTo('PlayerIcon.Styles')).position('absolute').top('50%').left('50%').translate('-50% -50%').color('green').fontSize('3em').width('60%').height('60%').paddingTop('0%').props).props),
     )
 })
 
@@ -25,14 +27,14 @@ const MainPage_BoardPiecesItem = React.memo(function MainPage_BoardPiecesItem(pr
 function MainPage(props) {
     const pathTo = name => props.path + '.' + name
     const {Page, Calculation, Data, Timer, TextElement, Dialog, Button, Block, ItemSet} = Elemento.components
-    const {Floor, And, IsNull, ForEach, Range, Or, Not, If, RandomListFrom, RandomFrom, ListContains, WithUpdates, ItemAt, Select, Ceiling} = Elemento.globalFunctions
+    const {Floor, And, IsNull, ForEach, Range, Or, Not, If, RandomListFrom, RandomFrom, ListContains, Random, WithUpdates, ItemAt, Select, Ceiling} = Elemento.globalFunctions
     const {Set, Reset} = Elemento.appFunctions
     const _state = Elemento.useGetStore()
     const Rows = _state.setObject(pathTo('Rows'), new Calculation.State(stateProps(pathTo('Rows')).value(8).props))
     const Columns = _state.setObject(pathTo('Columns'), new Calculation.State(stateProps(pathTo('Columns')).value(5).props))
     const NumberOfPositions = _state.setObject(pathTo('NumberOfPositions'), new Calculation.State(stateProps(pathTo('NumberOfPositions')).value(Rows * Columns).props))
     const NumberOfObstacles = _state.setObject(pathTo('NumberOfObstacles'), new Calculation.State(stateProps(pathTo('NumberOfObstacles')).value(Floor(NumberOfPositions * 0.6)).props))
-    const PointsAllowed = _state.setObject(pathTo('PointsAllowed'), new Calculation.State(stateProps(pathTo('PointsAllowed')).value(50).props))
+    const PointsAllowed = _state.setObject(pathTo('PointsAllowed'), new Calculation.State(stateProps(pathTo('PointsAllowed')).value(40).props))
     const BoardPositions = _state.setObject(pathTo('BoardPositions'), new Data.State(stateProps(pathTo('BoardPositions')).value(ForEach(Range(0, NumberOfPositions - 1), ($item, $index) => 'empty')).props))
     const PlayerPosition = _state.setObject(pathTo('PlayerPosition'), new Calculation.State(stateProps(pathTo('PlayerPosition')).value(BoardPositions.value.indexOf('player')).props))
     const FromPosition = _state.setObject(pathTo('FromPosition'), new Data.State(stateProps(pathTo('FromPosition')).value(null).props))
@@ -64,7 +66,7 @@ function MainPage(props) {
         let possibleObstaclePositions = Range(0, lastPosition - Columns)
         let obstaclePositions = RandomListFrom(possibleObstaclePositions, NumberOfObstacles)
         let playerPosition = RandomFrom(Range(NumberOfPositions - Columns, lastPosition))
-        let board = ForEach(Range(0, lastPosition), ($item, $index) => If(ListContains(obstaclePositions, $index), 'obstacle', () => If($index == playerPosition, 'player', 'empty')))
+        let board = ForEach(Range(0, lastPosition), ($item, $index) => If(ListContains(obstaclePositions, $index), () => Random(3, 9), () => If($index == playerPosition, 'player', 'empty')))
         Set(BoardPositions, board)
         return Reset(PointsUsed)
     }), [NumberOfPositions, Columns, NumberOfObstacles, BoardPositions, PointsUsed]))
@@ -110,16 +112,20 @@ function MainPage(props) {
         await Move(FromPosition, ToPosition)
     }), [Move, FromPosition, ToPosition])
     const MoveTimer1 = _state.setObject(pathTo('MoveTimer1'), new Timer.State(stateProps(pathTo('MoveTimer1')).period(0.7).endAction(MoveTimer1_endAction).props))
+    const ItemType = _state.setObject(pathTo('ItemType'), React.useCallback(wrapFn(pathTo('ItemType'), 'calculation', (position) => {
+        let item = ItemAt(BoardPositions, position)
+        return If(item == 'empty', 'empty', () => If(item == 'player', 'player', 'obstacle'))
+    }), [BoardPositions]))
     const CanMoveTo = _state.setObject(pathTo('CanMoveTo'), React.useCallback(wrapFn(pathTo('CanMoveTo'), 'calculation', (position) => {
         let difference = Math.abs(position - PlayerPosition)
-        let newPositionEmpty = ItemAt(BoardPositions, position) == 'empty'
+        let newPositionEmpty = ItemType(position) == 'empty'
         return And(newPositionEmpty, Or(difference == 1, difference == Columns))
-    }), [PlayerPosition, BoardPositions, Columns]))
+    }), [PlayerPosition, ItemType, Columns]))
     const CanMoveDiagonalTo = _state.setObject(pathTo('CanMoveDiagonalTo'), React.useCallback(wrapFn(pathTo('CanMoveDiagonalTo'), 'calculation', (position) => {
         let difference = Math.abs(position - PlayerPosition)
-        let newPositionEmpty = ItemAt(BoardPositions, position) == 'empty'
+        let newPositionEmpty = ItemType(position) == 'empty'
         return And(newPositionEmpty, Or(difference ==  Columns + 1, difference == Columns - 1))
-    }), [PlayerPosition, BoardPositions, Columns]))
+    }), [PlayerPosition, ItemType, Columns]))
     const ObstacleMove = _state.setObject(pathTo('ObstacleMove'), React.useCallback(wrapFn(pathTo('ObstacleMove'), 'calculation', (from, to) => {
         Set(FromPosition, from)
         Set(ToPosition, to)
@@ -128,13 +134,14 @@ function MainPage(props) {
     }), [FromPosition, ToPosition, MoveTimer1, MoveTimer2]))
     const RandomPosition = _state.setObject(pathTo('RandomPosition'), React.useCallback(wrapFn(pathTo('RandomPosition'), 'calculation', (positionType, excludePosition) => {
         let allPositions = Range(0, NumberOfPositions - 1)
-        let possiblePositions = Select(allPositions, ($item, $index) => ItemAt(BoardPositions, $item) == positionType && $item != excludePosition)
+        let possiblePositions = Select(allPositions, ($item, $index) => ItemType($item) == positionType && $item != excludePosition)
         return RandomFrom(possiblePositions)
-    }), [NumberOfPositions, BoardPositions]))
+    }), [NumberOfPositions, ItemType]))
     const DoPlayerObstacleMove = _state.setObject(pathTo('DoPlayerObstacleMove'), React.useCallback(wrapFn(pathTo('DoPlayerObstacleMove'), 'calculation', (position) => {
+        let cost = ItemAt(BoardPositions, position)
         ObstacleMove(position, RandomPosition('empty'))
-        return Set(PointsUsed, PointsUsed + 5)
-    }), [ObstacleMove, RandomPosition, PointsUsed]))
+        return Set(PointsUsed, PointsUsed + cost)
+    }), [BoardPositions, ObstacleMove, RandomPosition, PointsUsed]))
     const DoPlayerMove = _state.setObject(pathTo('DoPlayerMove'), React.useCallback(wrapFn(pathTo('DoPlayerMove'), 'calculation', (position, moveCost) => {
         Move(PlayerPosition, position)
         ObstacleMove(RandomPosition('obstacle'), RandomPosition('empty', position))
@@ -143,8 +150,8 @@ function MainPage(props) {
     const PlayerMove = _state.setObject(pathTo('PlayerMove'), React.useCallback(wrapFn(pathTo('PlayerMove'), 'calculation', (position) => {
         If(CanMoveTo(position), () => DoPlayerMove(position, 1))
         If(CanMoveDiagonalTo(position), () => DoPlayerMove(position, 3))
-        return If(ItemAt(BoardPositions, position) == 'obstacle', () => DoPlayerObstacleMove(position))
-    }), [CanMoveTo, DoPlayerMove, CanMoveDiagonalTo, BoardPositions, DoPlayerObstacleMove]))
+        return If(ItemType(position) == 'obstacle', () => DoPlayerObstacleMove(position))
+    }), [CanMoveTo, DoPlayerMove, CanMoveDiagonalTo, ItemType, DoPlayerObstacleMove]))
     const Instructions = _state.setObject(pathTo('Instructions'), new Dialog.State(stateProps(pathTo('Instructions')).initiallyOpen(false).props))
     const StatsLayout = _state.setObject(pathTo('StatsLayout'), new Block.State(stateProps(pathTo('StatsLayout')).props))
     const ReadyPanel = _state.setObject(pathTo('ReadyPanel'), new Block.State(stateProps(pathTo('ReadyPanel')).props))
@@ -213,13 +220,13 @@ function MainPage(props) {
         React.createElement(Timer, elProps(pathTo('GameTimer')).show(false).props),
         React.createElement(TextElement, elProps(pathTo('Title')).styles(elProps(pathTo('Title.Styles')).fontSize(24).fontFamily('fantasy, Arial').color('#039a03').props).content('Moving Maze').props),
         React.createElement(Dialog, elProps(pathTo('Instructions')).layout('vertical').showCloseButton(true).styles(elProps(pathTo('Instructions.Styles')).padding('2em').props).props,
-            React.createElement(TextElement, elProps(pathTo('InstructionsText')).allowHtml(true).content(`Move your piece - the smiley face - to the top row of the board, while keeping as many points as possible.
+            React.createElement(TextElement, elProps(pathTo('InstructionsText')).allowHtml(true).content(`Move the smiley face to the top row of the board, while keeping as many points as possible.
 
 
 Click on an empty space next to your smiley to move it.  Horizontal or vertical moves cost 1 point, diagonal moves cost 3 points. 
 
 
-Click on an obstacle to make it move somewhere else - this costs 5 points.
+Click on an obstacle to make it move somewhere else - this costs the number of points on the obstacle.
 
 
 
@@ -232,7 +239,7 @@ You score as many points as you have left when you get to the top row.
 Click New Maze to start again.
 
 
-You have 3 minutes to complete as many boards as you can.`).props),
+You have 3 minutes to complete as many mazes as you can.`).props),
             React.createElement(Button, elProps(pathTo('StartGame2')).content('Start Game').appearance('filled').show(Not(GameRunning)).action(StartGame2_action).props),
     ),
         React.createElement(Block, elProps(pathTo('StatsLayout')).layout('horizontal wrapped').styles(elProps(pathTo('StatsLayout.Styles')).fontSize('24').justifyContent('space-between').width('100%').props).props,
@@ -244,13 +251,13 @@ You have 3 minutes to complete as many boards as you can.`).props),
             React.createElement(TextElement, elProps(pathTo('Title')).styles(elProps(pathTo('Title.Styles')).color('#039a03').fontFamily('Chelsea Market').fontSize('28').props).content('Welcome!').props),
             React.createElement(TextElement, elProps(pathTo('ReadyText')).styles(elProps(pathTo('ReadyText.Styles')).fontSize('20').props).content(`Move your smiley face to the top of the board while keeping as many points as possible.
 
-Click on empty adjacent squares to move your piece, or click on an obstacle to move it.
+Click on empty adjacent squares to move the smiley, or click on an obstacle to move it.
 
 Click Instructions for full details
 
 Or Start Game to dive straight in!`).props),
     ),
-        React.createElement(Block, elProps(pathTo('PlayPanel')).layout('vertical').show(Or(Status == 'Playing', Status == 'Ended')).styles(elProps(pathTo('PlayPanel.Styles')).width('100%').padding('0').props).props,
+        React.createElement(Block, elProps(pathTo('PlayPanel')).layout('vertical').show(Or(Status == 'Playing', Status == 'Ended')).styles(elProps(pathTo('PlayPanel.Styles')).width('100%').padding('0').position('relative').props).props,
             React.createElement(Block, elProps(pathTo('GameBoard')).layout('horizontal wrapped').styles(elProps(pathTo('GameBoard.Styles')).width('100%').maxWidth('400px').aspectRatio(Columns/Rows).gap('0').border('1px solid blue').backgroundColor('blue').props).props,
             React.createElement(ItemSet, elProps(pathTo('BoardPieces')).itemContentComponent(MainPage_BoardPiecesItem).props),
     ),
@@ -258,7 +265,7 @@ Or Start Game to dive straight in!`).props),
             React.createElement(TextElement, elProps(pathTo('RoundWon')).show(IsRoundWon).content('You made it! ' + BoardPointsRemaining + ' points added').props),
             React.createElement(TextElement, elProps(pathTo('RoundFailed')).show(IsRoundFailed).content('Sorry - ').props),
             React.createElement(TextElement, elProps(pathTo('RoundSkipped')).show(RoundSkipped).content('Skipped').props),
-            React.createElement(Block, elProps(pathTo('EndedPanel')).layout('vertical').show(Status == 'Ended').props,
+            React.createElement(Block, elProps(pathTo('EndedPanel')).layout('vertical').show(Status == 'Ended').styles(elProps(pathTo('EndedPanel.Styles')).position('absolute').left('40%').translate('-50% -50%').top('50%').backgroundColor('lightblue').padding('1em').borderRadius('10px').border('2px solid blue').opacity('1').minWidth('20em').props).props,
             React.createElement(TextElement, elProps(pathTo('Title')).styles(elProps(pathTo('Title.Styles')).fontFamily('fantasy, Arial').fontSize('28').color('#039a03').props).content('Congratulations!').props),
             React.createElement(TextElement, elProps(pathTo('Score')).content('You have scored ' + Score + ' points!').props),
             React.createElement(TextElement, elProps(pathTo('Whatnext')).content('Click Start Game to have another go').props),
